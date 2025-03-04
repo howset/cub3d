ca7506c9253487ff945388b06f8c2a83241054d9
Reem Essam <60492880+ReemEssamEldin@users.noreply.github.com>
2025-03-04 14:10:27 +0100

    header file fixed

--- src/cub3d.h
+++ src/cub3d.h
@@ -8,6 +8,11 @@
 # include <X11/X.h> //keys
 # include <X11/keysym.h> //keys
 # include <math.h>
+#ifdef __APPLE__
+# include <mlx.h>
+#else
+# include "../lib/minilibx-linux/mlx.h"
+#endif
 # include "../lib/minilibx-linux/mlx.h"
 # include "../lib/src/libft/libft.h"
 # include "../lib/src/get_next_line/get_next_line.h"
