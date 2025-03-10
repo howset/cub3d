3d491af47666cda1ebeb9fa3b52f91ce91715dc8
howsetya <howsetya@googlemail.com>
2025-03-10 17:45:55 +0100

    Audio module for mac using afplay, but untested

--- src/audio/audio.c
+++ src/audio/audio.c
@@ -1,48 +1,31 @@
 #include "cub3d.h"
 #include <sys/wait.h>
 
-/* void	play_audio(char *audio_file)
+char	*detect_audioplayer(void)
 {
-	int		pid;
-	char	**argv;
+	char	*audio_player;
 
-	argv = ft_calloc(5, sizeof(char *));
-	argv[0] = ft_strdup("aplay");
-	//argv[2] = ft_itoa(loudness);
-	argv[1] = ft_strdup(audio_file);
-	//argv[2] = ft_strdup("-v 1");
-	pid = fork();
-	// if (!pid)
-	// {
-	// 	execve("/usr/bin/aplay", argv, NULL);
-	// 	exit(0);
-	// }
-	if (pid == 0)
-	{
-		if (execve("/usr/bin/aplay", argv, NULL) == -1)
-		{
-			perror("Error executing aplay");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else if (pid < 0)
-		perror("Error forking process");
-	free(argv[0]);
-	free(argv[1]);
-	//free(argv[2]);
-//	free(argv[3]);
-	free(argv);
-} */
+	#ifdef __APPLE__
+		aud_play = ft_strdup("afplay");
+	#else
+		audio_player = ft_strdup("aplay");
+	#endif
+	return (audio_player);
+}
 
 void play_audio(char *audio_file)
 {
 	int			pid;
 	char		**argv;
 	extern char	**environ;
+	char		*audio_player;
+	char		*path;
 
-	argv = ft_calloc(5, sizeof(char *));
-	argv[0] = ft_strdup("aplay");
+	audio_player = detect_audioplayer();
+	argv = ft_calloc(3, sizeof(char *));
+	argv[0] = ft_strdup(audio_player);
 	argv[1] = ft_strdup(audio_file);
+	path = ft_strjoin("/usr/bin/", audio_player);
 	pid = fork();
 	if (pid < 0)
 	{
@@ -51,7 +34,7 @@ void play_audio(char *audio_file)
 	}
 	else if (pid == 0)
 	{
-		if (execve("/usr/bin/aplay", argv, environ) == -1)
+		if (execve(path, argv, environ) == -1)
 		{
 			perror("execve");
 			exit(1);
@@ -59,9 +42,11 @@ void play_audio(char *audio_file)
 	}
 	else
 		waitpid(pid, NULL, WNOHANG);
+	free(audio_player);
 	free(argv[0]);
 	free(argv[1]);
 	free(argv);
+	free(path);
 }
 
 void	end_audio(void)
@@ -69,11 +54,13 @@ void	end_audio(void)
 	int		pid;
 	char	**argv;
 	extern char	**environ;
+	char		*audio_player;
 
+	audio_player = detect_audioplayer();
 	argv = ft_calloc(4, sizeof(char *));
 	argv[0] = ft_strdup("killall");
 	argv[1] = ft_strdup("-9");
-	argv[2] = ft_strdup("aplay");
+	argv[2] = ft_strdup(audio_player);
 	pid = fork();
 	if (pid == 0)
 	{
@@ -81,7 +68,8 @@ void	end_audio(void)
 		exit(0);
 	}
 	else if (pid > 0)
-		waitpid(pid, NULL, 0); // Wait for killall to complete
+		waitpid(pid, NULL, 0);
+	free(audio_player);
 	free(argv[0]);
 	free(argv[1]);
 	free(argv[2]);
