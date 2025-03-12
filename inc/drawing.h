8ed667c4aba036640846d3d5b60534cbc8adb944
howsetya <howsetya@googlemail.com>
2025-03-12 17:59:25 +0100

    Merge h-wall_collision

--- inc/drawing.h
+++ inc/drawing.h
@@ -8,6 +8,7 @@ void	put_pixel(int x, int y, int color, t_data *cub3d);
 void	draw_line(t_player *player, t_data *cub3d, float start_x, int i);
 int		draw_loop(t_data *cub3d);
 void	draw_square(int x, int y, int size, int color, t_data *cub3d);
+void	draw_triangle(int x, int y, int size, int color, t_data *cub3d);
 void	draw_map(t_data *cub3d);
 void	clear_image(t_data *cub3d);
 // math.c
