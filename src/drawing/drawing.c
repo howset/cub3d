7734d79d35608c5e051084953a859504b9e36210
Reem Essameldin <reem.essam.eldin@gmail.com>
2025-03-13 13:56:59 +0100

    some norminette

--- src/drawing/drawing.c
+++ src/drawing/drawing.c
@@ -6,7 +6,7 @@
 /*   By: reldahli <reldahli@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 13:12:30 by reldahli          #+#    #+#             */
-/*   Updated: 2025/03/13 13:18:44 by reldahli         ###   ########.fr       */
+/*   Updated: 2025/03/13 13:54:09 by reldahli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -223,6 +223,12 @@ void	draw_triangle(int x, int y, int size, int color, t_data *cub3d)
 	int	y2;
 	int	x3;
 	int	y3;
+	int	tx;
+	int	ty;
+	int	rx;
+	int	ry;
+	int	bx;
+	int	by;
 
 	x1 = x;
 	y1 = y;
@@ -234,16 +240,16 @@ void	draw_triangle(int x, int y, int size, int color, t_data *cub3d)
 	for (int i = 0; i <= size; i++)
 	{
 		// Top left edge: from (x1, y1) to (x2, y2)
-		int	tx = x1 + (x2 - x1) * i / size;
-		int	ty = y1 + (y2 - y1) * i / size;
+		tx = x1 + (x2 - x1) * i / size;
+		ty = y1 + (y2 - y1) * i / size;
 		put_pixel(tx, ty, color, cub3d);
 		// Top right edge: from (x1, y1) to (x3, y3)
-		int	rx = x1 + (x3 - x1) * i / size;
-		int	ry = y1 + (y3 - y1) * i / size;
+		rx = x1 + (x3 - x1) * i / size;
+		ry = y1 + (y3 - y1) * i / size;
 		put_pixel(rx, ry, color, cub3d);
 		// Bottom edge: from (x2, y2) to (x3, y3)
-		int	bx = x2 + (x3 - x2) * i / size;
-		int	by = y2 + (y3 - y2) * i / size;
+		bx = x2 + (x3 - x2) * i / size;
+		by = y2 + (y3 - y2) * i / size;
 		put_pixel(bx, by, color, cub3d);
 	}
 }
