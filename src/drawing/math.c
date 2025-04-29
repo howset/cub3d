dadf85472883e77dec8e1647c6dbdcef9ea8cebe
howsetya <howsetya@googlemail.com>
2025-04-29 16:09:37 +0200

    Change player and ray pixel locations

--- src/drawing/math.c
+++ src/drawing/math.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   math.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: reldahli <reldahli@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 13:12:39 by reldahli          #+#    #+#             */
-/*   Updated: 2025/03/13 13:48:21 by reldahli         ###   ########.fr       */
+/*   Updated: 2025/04/29 15:50:52 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 	return (false);
 } */
 
-bool	touch(float px, float py, t_data *cub3d)
+/* bool	touch(float px, float py, t_data *cub3d)
 {
 	int	x;
 	int	y;
@@ -43,6 +43,32 @@ bool	touch(float px, float py, t_data *cub3d)
 		&& cub3d->map_info.map[y][x] == '1')
 		return (true);
 	return (false);
+} */
+
+bool	touch(float px, float py, t_data *cub3d)
+{
+    int	x;
+    int	y;
+
+    // Check if position is outside the map boundaries
+    if (px < 0 || py < 0)
+        return (true);
+
+    // Adjust px and py slightly to ensure proper rounding for south/east edges
+    x = (int)((px + 0.001) / BLOCK);
+    y = (int)((py + 0.001) / BLOCK);
+
+    // Check if position is outside map array bounds
+    if (y >= cub3d->map_info.map_rows || x >= cub3d->map_info.map_cols
+        || y < 0 || x < 0)
+        return (true);
+
+    // Check if position has a wall
+    if (cub3d->map_info.map[y] && x < (int)ft_strlen(cub3d->map_info.map[y])
+        && cub3d->map_info.map[y][x] == '1')
+        return (true);
+
+    return (false);
 }
 
 float	distance(float x, float y)
