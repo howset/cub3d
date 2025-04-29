eb0ea7fefdf780ab534a4153a82390d308aeb357
Reem Essameldin <reem.essam.eldin@gmail.com>
2025-04-29 13:50:57 +0200

    some norm stuff to clesn the code a little bit

--- src/drawing/drawing.c
+++ src/drawing/drawing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   drawing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: reldahli <reldahli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: reldahli <reldahli@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 13:12:30 by reldahli          #+#    #+#             */
-/*   Updated: 2025/04/25 16:29:58 by reldahli         ###   ########.fr       */
+/*   Updated: 2025/04/29 13:48:19 by reldahli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,56 +43,50 @@ void	put_pixel(int x, int y, int color, t_data *cub3d)
 
 void	cast_ray(t_data *cub3d, float start_x, float *ray_x, float *ray_y)
 {
-    float	cos_angle;
-    float	sin_angle;
-    float	max_ray_length;
-    float	current_length;
+	float	cos_angle;
+	float	sin_angle;
+	float	max_ray_length;
+	float	current_length;
 
-    cos_angle = cos(start_x);
-    sin_angle = sin(start_x);
-    *ray_x = cub3d->player.x + BLOCK / 4;  // Start from middle of player square
-    *ray_y = cub3d->player.y + BLOCK / 4;  // Start from middle of player square
-    max_ray_length = BLOCK * 5;      // Limit ray to 5 blocks length
-    current_length = 0;
-
-    while (!touch(*ray_x, *ray_y, cub3d) && current_length < max_ray_length)
-    {
-        put_pixel(*ray_x, *ray_y, RED, cub3d); // This line handles rays
-        *ray_x += cos_angle;
-        *ray_y += sin_angle;
-        current_length += 1;
-    }
+	cos_angle = cos(start_x);
+	sin_angle = sin(start_x);
+	*ray_x = cub3d->player.x + BLOCK / 4; // Start from middle of player square
+	*ray_y = cub3d->player.y + BLOCK / 4; // Start from middle of player square
+	max_ray_length = BLOCK * 5; // Limit ray to 5 blocks length
+	current_length = 0;
+	while (!touch(*ray_x, *ray_y, cub3d) && current_length < max_ray_length)
+	{
+		put_pixel(*ray_x, *ray_y, RED, cub3d); // This line handles rays
+		*ray_x += cos_angle;
+		*ray_y += sin_angle;
+		current_length += 1;
+	}
 }
 
 void	draw_line(t_player *player, t_data *cub3d, float start_x, int i)
 {
-    float	ray_x;
-    float	ray_y;
-    float	dist;
-    float	height;
-    int		start_y;
-    int		end;
-    int		col_wall;
-    int		col_ceil;
-    int		col_floo;
+	float	ray_x;
+	float	ray_y;
+	float	dist;
+	float	height;
+	int		start_y;
+	int		end;
+	int		col_wall;
+	int		col_ceil;
+	int		col_floo;
 
-    //(void)i;
-
-    // Cast the ray
-    cast_ray(cub3d, start_x, &ray_x, &ray_y);
-
-    // Handle first-person perspective
-    dist = fixed_dist(player->x, player->y, ray_x, ray_y, cub3d); // Fix fish-eye
-    height = (BLOCK / dist) * (WID / 2);
-    start_y = (HEI - height) / 2;
-    end = start_y + height;
-    col_wall = GRE;
-    col_ceil = rgb_to_colour(cub3d->map_info.ce_col);
-    col_floo = rgb_to_colour(cub3d->map_info.fl_col);
-
-    draw_maindisplay(0, start_y, col_ceil, cub3d, i);
-    draw_maindisplay(start_y, end, col_wall, cub3d, i);
-    draw_maindisplay(end, HEI, col_floo, cub3d, i);
+	cast_ray(cub3d, start_x, &ray_x, &ray_y);
+	// Handle first-person perspective
+	dist = fixed_dist(player->x, player->y, ray_x, ray_y, cub3d);
+	height = (BLOCK / dist) * (WID / 2);
+	start_y = (HEI - height) / 2;
+	end = start_y + height;
+	col_wall = GRE;
+	col_ceil = rgb_to_colour(cub3d->map_info.ce_col);
+	col_floo = rgb_to_colour(cub3d->map_info.fl_col);
+	draw_maindisplay(0, start_y, col_ceil, cub3d, i);
+	draw_maindisplay(start_y, end, col_wall, cub3d, i);
+	draw_maindisplay(end, HEI, col_floo, cub3d, i);
 }
 
 void	draw_maindisplay(int top, int bot, int colour, t_data *cub3d, int i)
@@ -111,11 +105,15 @@ int	draw_loop(t_data *cub3d)
 	float	fraction;
 	float	start_x;
 	int		i;
-	float   ray_angles[WID];  // Store ray angles for later use
-
+	float	cos_angle;
+	float	sin_angle;
+	float	ray_x;
+	float	ray_y;
+	float	max_ray_length;
+	float	current_length;
+	float	ray_angles[WID]; // Store ray angles for later use
 	move_player(&cub3d->player, cub3d);
 	clear_image(cub3d);
-
 	// Calculate and store ray angles
 	fraction = PI / 3 / WID;
 	start_x = cub3d->player.angle - PI / 6;
@@ -127,21 +125,17 @@ int	draw_loop(t_data *cub3d)
 		start_x += fraction;
 		i++;
 	}
-
 	// Draw minimap
 	draw_map(cub3d);
-
 	// Redraw rays on top of minimap
-	float cos_angle, sin_angle, ray_x, ray_y;
-	float max_ray_length = BLOCK * 5;
-	float current_length;
-
-	for (i = 0; i < WID; i++)
+	max_ray_length = BLOCK * 5;
+	i = 0;
+	while (i < WID)
 	{
 		cos_angle = cos(ray_angles[i]);
 		sin_angle = sin(ray_angles[i]);
-		ray_x = cub3d->player.x + BLOCK/4;
-		ray_y = cub3d->player.y + BLOCK/4;
+		ray_x = cub3d->player.x + BLOCK / 4;
+		ray_y = cub3d->player.y + BLOCK / 4;
 		current_length = 0;
 		while (!touch(ray_x, ray_y, cub3d) && current_length < max_ray_length)
 		{
@@ -150,31 +144,32 @@ int	draw_loop(t_data *cub3d)
 			ray_y += sin_angle;
 			current_length += 1;
 		}
+		i++;
 	}
-	draw_filled_square(cub3d->player.x, cub3d->player.y, BLOCK/2, BLU, cub3d);
-
-	mlx_put_image_to_window(cub3d->mlx_ptr, cub3d->win_ptr,
+	draw_filled_square (cub3d->player.x, cub3d->player.y, BLOCK / 2,
+		BLU, cub3d);
+	mlx_put_image_to_window (cub3d->mlx_ptr, cub3d->win_ptr,
 		cub3d->img_ptr, 0, 0);
 	return (0);
 }
 
-// void	draw_square(int x, int y, int size, int color, t_data *cub3d)
-// {
-// 	for (int i = 0; i < size; i++)
-// 		put_pixel(x + i, y, color, cub3d);
-// 	for (int i = 0; i < size; i++)
-// 		put_pixel(x, y + i, color, cub3d);
-// 	for (int i = 0; i < size; i++)
-// 		put_pixel(x + size, y + i, color, cub3d);
-// 	for (int i = 0; i < size; i++)
-// 		put_pixel(x + i, y + size, color, cub3d);
-// }
-
 void	draw_filled_square(int x, int y, int size, int color, t_data *cub3d)
 {
-	for (int j = 0; j < size; j++)
-		for (int i = 0; i < size; i++)
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (j < size)
+	{
+		while (i < size)
+		{
 			put_pixel(x + i, y + j, color, cub3d);
+			i++;
+		}
+		i = 0;
+		j++;
+	}
 }
 
 void	draw_map(t_data *cub3d)
@@ -184,16 +179,17 @@ void	draw_map(t_data *cub3d)
 	int		map_height;
 	int		wall_color;
 	int		bg_color;
-	int 	space_color;
-	int map_padding;
+	int		space_color;
+	int		map_padding;
+	int		len;
 
 	map = cub3d->map_info.map;
 	map_height = 0;
 	map_width = 0;
 	map_padding = 1;
+	len = 0;
 	while (map[map_height])
 	{
-		int len = 0;
 		while (map[map_height][len])
 			len++;
 		if (len > map_width)
@@ -202,7 +198,6 @@ void	draw_map(t_data *cub3d)
 	}
 	map_width += map_padding;
 	map_height += map_padding;
-
 	bg_color = GREY;
 	wall_color = YEL;
 	space_color = GREY;
@@ -213,14 +208,27 @@ void	draw_map(t_data *cub3d)
 	for (int y = 0; map[y]; y++)
 		for (int x = 0; map[y][x]; x++)
 			if (map[y][x] == '1')
-				draw_filled_square(x * BLOCK, y * BLOCK, BLOCK, wall_color, cub3d);
+				draw_filled_square(x * BLOCK, y * BLOCK, BLOCK,
+					wall_color, cub3d);
 			else if (map[y][x] == '0')
-				draw_filled_square(x * BLOCK, y * BLOCK, BLOCK, space_color, cub3d);
+				draw_filled_square(x * BLOCK, y * BLOCK, BLOCK,
+					space_color, cub3d);
 }
 
 void	clear_image(t_data *cub3d)
 {
-	for (int y = 0; y < HEI; y++)
-		for (int x = 0; x < WID; x++)
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < HEI)
+	{
+		x = 0;
+		while (x < WID)
+		{
 			put_pixel(x, y, 0, cub3d);
+			x++;
+		}
+		y++;
+	}
 }
