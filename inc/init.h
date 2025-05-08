23f9d21b28485b6ed7b0923da5f3e04b3442b9dd
howset <howsetya@googlemail.com>
2025-05-08 16:27:40 +0200

    Small cleanup on init

--- inc/init.h
+++ inc/init.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: reldahli <reldahli@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: hsetya <hsetya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:39:59 by reldahli          #+#    #+#             */
-/*   Updated: 2025/03/13 11:40:00 by reldahli         ###   ########.fr       */
+/*   Updated: 2025/05/08 16:27:08 by hsetya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@
 
 void		init_cub3d(t_data *cub3d);
 void		init_mlx(t_data *cub3d);
-//void		init_player(t_player *player); //need to take cub3d
 void		init_player(t_data *cub3d);
 #endif
