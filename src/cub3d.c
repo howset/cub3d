fddf4c19ea774d9486136b594db6298a83e2baf5
howsetya <howsetya@googlemail.com>
2025-05-09 17:48:17 +0200

    unsuccessful attempt to clean up textures

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 13:41:51 by reldahli          #+#    #+#             */
-/*   Updated: 2025/05/06 17:33:16 by hsetyamu         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:43:33 by hsetyamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,10 @@ int	main(int argc, char *argv[])
 	key_hooks(&cub3d);
 	mlx_loop_hook(cub3d.mlx_ptr, render, &cub3d);
 	mlx_loop(cub3d.mlx_ptr);
-	clean_mapheader(&cub3d.map_info);
-	clean_mapcontent(&cub3d.map_info);
+	// clean_mapheader(&cub3d.map_info);
+	// clean_mapcontent(&cub3d.map_info);
+	// clean_textures(&cub3d);
+	clean_all(&cub3d);
 	return (0);
 }
 
---
+++ src/utils/cleaning.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleaning.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 17:38:57 by hsetyamu          #+#    #+#             */
+/*   Updated: 2025/05/09 17:44:06 by hsetyamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/cub3d.h"
+
+void	clean_all(t_data *cub3d);
+void	clean_mapheader(t_map *map_info);
+void	clean_mapcontent(t_map *map_info);
+void	clean_textures(t_data *cub3d);
+
+void	clean_all(t_data *cub3d)
+{
+	if (cub3d)
+	{
+		clean_mapheader(&cub3d->map_info);
+		clean_mapcontent(&cub3d->map_info);
+		clean_textures(cub3d);
+		if (cub3d->win_ptr)
+			mlx_destroy_window(cub3d->mlx_ptr, cub3d->win_ptr);
+		if (cub3d->mlx_ptr)
+		{
+			mlx_destroy_display(cub3d->mlx_ptr);
+			free(cub3d->mlx_ptr);
+		}
+	}
+}
+
+void	clean_mapheader(t_map *map_info)
+{
+	if (map_info->no_tex)
+		free(map_info->no_tex);
+	if (map_info->so_tex)
+		free(map_info->so_tex);
+	if (map_info->ea_tex)
+		free(map_info->ea_tex);
+	if (map_info->we_tex)
+		free(map_info->we_tex);
+	if (map_info->fl_col)
+		free(map_info->fl_col);
+	if (map_info->ce_col)
+		free(map_info->ce_col);
+}
+
+void	clean_mapcontent(t_map *map_info)
+{
+	int	i;
+
+	if (map_info->map == NULL)
+		return ;
+	i = 0;
+	while (map_info->map[i] != NULL)
+	{
+		free(map_info->map[i]);
+		i++;
+	}
+	free(map_info->map);
+}
+
+void	clean_textures(t_data *cub3d)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (cub3d->textures[i].img)
+			mlx_destroy_image(cub3d->mlx_ptr, cub3d->textures[i].img);
+		// if (cub3d->textures[i].addr)
+		// 	free(cub3d->textures[i].addr);
+		i++;
+	}
+}
